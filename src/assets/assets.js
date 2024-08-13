c45b7101edfa60a5df7baf44645f85a848ef8189
Manisha varma <manishavkapex1@gmail.com>
2024-08-13 14:39:34 +0530

    add icons path

---
+++ src/assets/assets.js
@@ -0,0 +1,37 @@
+import bulb_icon from "./bulb.png";
+import code_icon from "./code.png";
+import comment_icon from "./comment.png";
+import compass_icon from "./compass.png";
+import gallery_icon from "./gallery.png";
+import user_icon from "./girl.png";
+import history_icon from "./history.png";
+import menu_icon from "./menu.png";
+import mic_icon from "./mic.png";
+import photo_icon from "./photo.png";
+import plus_icon from "./plus.png";
+import question_icon from "./question.png";
+import send_icon from "./send.png";
+import settings_icon from "./settings.png";
+import youtube_icon from "./youtube.png";
+import gemini_icon from "./gemini-icon.png";
+import geminiS_icon from "./gemini-icon2.png";
+
+export const assets = {
+  bulb_icon,
+  code_icon,
+  comment_icon,
+  compass_icon,
+  gallery_icon,
+  user_icon,
+  history_icon,
+  menu_icon,
+  mic_icon,
+  photo_icon,
+  plus_icon,
+  question_icon,
+  send_icon,
+  settings_icon,
+  youtube_icon,
+  geminiS_icon,
+  gemini_icon,
+};
