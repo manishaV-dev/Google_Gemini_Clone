ba1d4fa32b9d40556bb5430fa51f24001ecbd38d
Manisha varma <manishavkapex1@gmail.com>
2025-02-01 23:37:30 +0530

    create main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Main from "./components/Main/Main"
 import Sidebar from "./components/Sidebar/Sidebar"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Sidebar />
+      <Main />
     </>
   )
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "./main.css";
+import { assets } from "../../assets/assets";
+
+const Main = () => {
+  return (
+    <>
+      <div className="main">
+        <div className="nav">
+          <p>Gemini</p>
+          <img src={assets.user_icon} alt="user-icon" />
+        </div>
+
+        <div className="main-container">
+          <div className="greet">
+            <p>
+              <span>Hello, Coderbunny.</span>
+            </p>
+            <p>How can I help you today?</p>
+          </div>
+          <div className="cards">
+            <div className="card">
+              <p>Explain about the Mahakumbh Mela 2025</p>
+              <img src={assets.question_icon} alt="" />
+            </div>
+
+            <div className="card">
+              <p>Briefly summerize this concept: Event Loop</p>
+              <img src={assets.bulb_icon} alt="" />
+            </div>
+
+            <div className="card">
+              <p>Suggest top 10 places to visit in summer in India</p>
+              <img src={assets.compass_icon} alt="" />
+            </div>
+
+            <div className="card">
+              <p>Improve the readability of the following code</p>
+              <img src={assets.code_icon} alt="" />
+            </div>
+          </div>
+
+          <div className="main-bottom">
+            <div className="search-box">
+              <input type="text" placeholder="Ask Gemini" />
+              <div>
+                <img src={assets.gallery_icon} alt="" />
+                <img src={assets.mic_icon} alt="" />
+                <img src={assets.send_icon} alt="" />
+              </div>
+            </div>
+
+            <p className="bottom-info">
+              Gemini may display inaccurate info, including about people, so
+              double check its response. Your privacy and Gemini Apps
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
